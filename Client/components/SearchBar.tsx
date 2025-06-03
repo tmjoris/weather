cff0e47e865b2315fed1f52cefced54f1212f167
tmjoris <tangajoris@gmail.com>
2025-06-03 13:20:30 +0300

    Added search bar

--- Client/components/SearchBar.tsx
+++ Client/components/SearchBar.tsx
@@ -7,4 +7,17 @@ export default function SearchBar({onSearch}: {onSearch: (city:string) => void})
         e.preventDefault();
         onSearch(city);
     };
+
+    return(
+        <form>
+            <input
+             type="text"
+             value={city}
+             onChange={(e) => setCity(e.target.value)}
+             className="input input-bordered w-full"
+             placeholder="Enter city"
+            />
+            <button type="submit" className="btn btn-primary">Search</button>
+        </form>
+    );
 }
