40eb0efc1206e3ac698cd58d4e3c941bb90f094b
tmjoris <tangajoris@gmail.com>
2025-06-03 16:03:03 +0300

    added units to weather types

--- Client/components/WeatherCard.tsx
+++ Client/components/WeatherCard.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
-import { WeatherData } from '@/types/Weather'; // adjust path if needed
+import { WeatherData } from '@/types/Weather';
 
 interface WeatherProps {
   data: WeatherData;
+  units: 'metric' | 'imperial';
 }
 
 
-const Weather: React.FC<WeatherProps> = ({ data }) => {
+const Weather: React.FC<WeatherProps> = ({ data, units }) => {
   console.log(data);
   return (
     <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-gray-300 z-10'>
