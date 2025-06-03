af924950d9b979f2d7c94f1026b23fe19cf27741
tmjoris <tangajoris@gmail.com>
2025-06-03 15:10:33 +0300

    fixed spinner import

--- Client/components/Spinner.tsx
+++ Client/components/Spinner.tsx
@@ -1,10 +1,9 @@
 import Image from 'next/image';
-import spinner from '../public/spinner.gif'
 
 const Spinner = () => {
     return (
         <>
-            <Image className='w-[200px] m-auto block' src={spinner} alt='loading...'/>
+            <Image className='w-[200px] m-auto block' src="/spinner.gif" alt="loading..."/>
         </>
     );
 };
